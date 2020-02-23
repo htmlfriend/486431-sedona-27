118dfc901795a782358b3bd30b2c0d71bc196736
yurii <yuriy.tim@yahoo.com>
2020-02-23 18:05:25 -0500

    finished work with form

--- js/app.js
+++ js/app.js
@@ -5,17 +5,25 @@ window.addEventListener("DOMContentLoaded", function() {
   var minusChildren = document.querySelector("#minus-children");
   var plusBtnChildren = document.querySelector("#plus-children");
   var field = document.querySelector("#appointment-children");
+
   var choiceBtn = document.querySelector("#choice");
   var slider = document.querySelector("#range");
   var output = document.querySelector("#value-to");
   var orderNow = document.querySelectorAll(".btn-handler");
   var modal = document.querySelector("#modal");
-
+  var storage = localStorage.getItem("appointments");
   if (choiceBtn) {
-    choiceBtn.addEventListener("click", function() {
+    choiceBtn.addEventListener("click", function(event) {
+      event.preventDefault();
       var choiceForm = document.querySelector("#formChoice");
+      var sendBtn = choiceForm.querySelector("#sendBtn");
       closePopup(choiceForm, "hide");
+      var appointmentDate = choiceForm.querySelector("#appointment-date");
+      appointmentDate.focus();
 
+      var appointmentDateOut = choiceForm.querySelector(
+        "#appointment-data-out"
+      );
       minusChildren.addEventListener("click", function() {
         field.value = parseInt(field.value) - 1;
       });
@@ -28,6 +36,25 @@ window.addEventListener("DOMContentLoaded", function() {
       });
       plusBtnAdult.addEventListener("click", function() {
         fieldAdult.value = parseInt(fieldAdult.value) + 1;
+      });
+      sendBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        if (appointmentDate.value != "") {
+          appointmentDate.classList.remove("error-message");
+          if (appointmentDateOut.value != "") {
+            appointmentDateOut.classList.remove("error-message");
+            if (fieldAdult.value > 0) {
+              fieldAdult.classList.remove("error-message");
+              choiceForm.submit();
+            } else {
+              fieldAdult.classList.add("error-message");
+            }
+          } else {
+            appointmentDateOut.classList.add("error-message");
+          }
+        } else {
+          appointmentDate.classList.add("error-message");
+        }
       });
     });
   }
